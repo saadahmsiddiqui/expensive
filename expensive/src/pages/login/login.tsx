19d1f5402d4ba125c69cdd9878b4a5ddff612a12
Saad Ahmed Siddiqui <saadahm.siddiqui@gmail.com>
2024-09-24 00:59:09 +0200

    style update

--- expensive/src/pages/login/login.tsx
+++ expensive/src/pages/login/login.tsx
@@ -2,12 +2,24 @@ import { Center, TextInput, Button, Box } from '@mantine/core';
 
 export function Login() {
   return (
-    <Center style={{ height: '100vh' }} bg="var(--mantine-color-gray-light)">
-      <Box>
-        <TextInput label="Email" placeholder="foo@bar.com" />
-        <TextInput mt={10} label="Password" placeholder="******" />
+    <Center style={{ height: '100vh' }} bg="var(--mantine-color-dark-6)">
+      <Box w={'25vw'}>
+        <TextInput
+          style={{ color: '#FFF' }}
+          label="Email"
+          placeholder="foo@bar.com"
+        />
 
-        <Button mt={10}>Login</Button>
+        <TextInput
+          style={{ color: '#FFF' }}
+          mt={10}
+          label="Password"
+          placeholder="******"
+        />
+
+        <Button w={'100%'} color="orange.6" mt={20}>
+          Login
+        </Button>
       </Box>
     </Center>
   );
