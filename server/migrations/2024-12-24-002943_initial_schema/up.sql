eb9fe221538540668b02e3a783463a0540251cb3
Saad Ahmed Siddiqui <saadahm.siddiqui@gmail.com>
2024-12-25 02:30:04 +0100

    attempt to write server in rust

--- server/migrations/2024-12-24-002943_initial_schema/up.sql
+++
@@ -1,7 +0,0 @@
--- Your SQL goes here
-CREATE TABLE Users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,26 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+
+use rocket::http::uri::Origin;
+use rocket::serde::json::{json, Value};
+
+const USERS_API_PREFIX: Origin<'static> = uri!("/users");
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> String {
+    String::from("Hello, world!")
+}
+
+#[get("/")]
+fn create_user_api() -> Value {
+    json!({
+        "name": "Saad Ahmed Siddiqui"
+    })
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index])
+        .mount(USERS_API_PREFIX, routes![create_user_api])
 }
